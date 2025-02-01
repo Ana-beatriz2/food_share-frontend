ceba7c3cfcc7da7dc9f04d0555e636c13e22a8e2
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2025-01-31 21:05:35 -0300

    alteracoes

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,5 +1,33 @@
-export default function Login() {
-    return (
-        <h1>Página de login</h1>
-    )
-}
+import React from "react";
+import Logo from "@/components/ui/logo";
+
+const LoginPage = () => {
+  return (
+    <div className="flex flex-col items-center pb-72 tracking-tighter text-yellow-800 bg-orange-50 max-md:pb-24">
+      <Logo size="large" />
+
+      <form className="flex flex-col items-center w-full">
+        <h1 className="mt-36 text-3xl font-bold text-lime-500 max-md:mt-10">
+          Seja bem-vindo(a)!
+        </h1>
+
+        <div className="mt-20 max-md:mt-10">
+          <InputField label="Email" type="email" />
+        </div>
+
+        <div className="mt-12 max-md:mt-10">
+          <InputField label="Senha" type="password" />
+        </div>
+
+        <button
+          type="submit"
+          className="px-16 pt-3.5 pb-5 mt-20 max-w-full text-2xl font-bold whitespace-nowrap bg-amber-200 shadow-lg w-[446px] max-md:px-5 max-md:mt-10"
+        >
+          CONTINUAR
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/pages/layouts/auth/authLayout.jsx
+++ src/pages/layouts/auth/authLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Logo from "@/components/ui/logo";
 
 export default function AuthLayout() {
-  return (
-    <div className="flex flex-col items-center pb-72 tracking-tighter text-yellow-800 bg-orange-50 max-md:pb-24">
-      <header className="flex z-10 flex-wrap gap-5 justify-between items-start self-stretch pr-20 pb-8 pl-8 w-full text-amber-200 whitespace-nowrap bg-lime-500 max-md:px-5 max-md:max-w-full">
-      </header>
-      <Outlet />
-    </div>
-  );
+    return(
+        <div>
+            <div> 
+                <h1>Olá</h1>
+            </div>
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    )
 }
