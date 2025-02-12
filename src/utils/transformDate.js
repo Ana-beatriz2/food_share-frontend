b599b8faeb1542362ce44899965ee74c17cb7ddc
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-02-12 01:01:58 -0300

    fix: adiciona imports que não estavam no local

--- src/utils/transformDate.js
+++ src/utils/transformDate.js
@@ -1,4 +1,5 @@
-
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function transformDate(date) {
     return format(new Date(date), 'dd/MM/yyyy', { locale: ptBR })
